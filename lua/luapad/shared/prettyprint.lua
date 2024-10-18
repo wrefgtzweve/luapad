9bedc035f7741a183278ab2f6e89d942ce7fa0fb
Redox <git@redox.to>
2024-10-18 15:40:47 +0200

    Rewrite table printer

--- lua/luapad/shared/prettyprint.lua
+++ lua/luapad/shared/prettyprint.lua
@@ -1,50 +1,69 @@
-local MAX_DEPTH = 3
+local MAX_TBL_LEN = 256
+local MAX_DEPTH = 10
 
-local function getIndent( depth )
-    local str = ""
-    for _ = 1, depth do
-        str = str .. "  "
+local converters = {
+    ["function"] = function( func )
+        local info = debug.getinfo( func )
+        local str = tostring( func )
+        return str .. " " .. info.short_src .. ":" .. info.linedefined
+    end,
+    ["string"] = function( str )
+        return string.format( "\"%s\"", str )
     end
-    return str
+}
+local function varToStr( var )
+    local t = type( var )
+    if converters[t] then
+        return converters[t]( var )
+    end
+
+    return tostring( var )
 end
 
-local function prettyTable( tbl, str, indent, done )
-    if not next( tbl ) then
-        return "{}"
+local function prettyTable( t, str, indent, depth, done )
+    indent = indent or 0
+    str = str or ""
+    depth = depth or 0
+    done = done or {}
+
+    if depth > MAX_DEPTH then
+        return str .. string.rep( "  ", indent ) .. "Max table depth reached\n"
     end
+    depth = depth + 1
 
-    if not str then
-        str = "{"
-        indent = 0
-        done = {}
-    end
+    local keys = table.GetKeys( t )
+    table.sort( keys, function( a, b )
+        if ( isnumber( a ) and isnumber( b ) ) then return a < b end
+        return tostring( a ) < tostring( b )
+    end )
 
-    if tbl == _G then
-        return str .. "\n" .. getIndent( indent ) .. "_G = _G,"
-    end
+    done[t] = true
 
-    if indent >= MAX_DEPTH then
-        return str .. "\n" .. getIndent( indent + 1 ) .. "Max table depth " .. tostring( tbl ) .. "," .. "\n" .. getIndent( indent ) .. "},"
-    end
+    local overMaxLen = #keys > MAX_TBL_LEN
+    for i = 1, #keys do
+        local key = keys[i]
+        local value = t[key]
+        key = ( type( key ) == "string" ) and "[\"" .. key .. "\"]" or "[" .. varToStr( key ) .. "]"
+        str = str .. string.rep( "  ", indent )
 
-    done[tbl] = true
+        if  istable( value ) and not done[value] then
 
-    indent = indent + 1
-    for k, v in pairs( tbl ) do
-        if istable( v ) then
-            if done[v] then
-                str = str .. "\n" .. getIndent( indent ) .. tostring( k ) .. " = Recursive " .. tostring( v ) .. ","
-            else
-                str = str .. "\n" .. getIndent( indent ) .. tostring( k ) .. " = {"
-                str = prettyTable( v, str, indent, done )
-            end
+            done[value] = true
+            str = str .. key .. ":\n"
+            str = str .. prettyTable( value, str, indent + 2, depth, done )
+            done[value] = nil
+
         else
-            str = str .. "\n" .. getIndent( indent ) .. tostring( k ) .. " = " .. tostring( v ) .. ","
+            str = str .. key .. "\t=\t" .. varToStr( value ) .. "\n"
+        end
+
+        if overMaxLen and i == MAX_TBL_LEN then
+            str = str .. string.rep( "  ", indent ) .. #keys - MAX_TBL_LEN .. " more elements...\n"
+            break
         end
     end
-    indent = indent - 1
 
-    return str .. "\n" .. getIndent( indent ) .. "},"
+    return str
 end
 
 function luapad.PrettyPrint( obj )
